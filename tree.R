aa87e6b4225b3032a41b5d2b798806c898479544
Ishak Yusuf <142504615+isky20@users.noreply.github.com>
2024-03-21 13:00:52 +0100

    Update tree.R

--- tree.R
+++ tree.R
@@ -6,7 +6,7 @@ library(gridExtra)
 library(ggtreeExtra)
 library(svglite)
 
-tree <- function(df, table_name,n = 10) { 
+tree <- function(df, table_name,n = 10, off= 1.5) { 
   # Loop through each column starting with 'score'
   for (col_name in names(df)[startsWith(names(df), "score")]) {
     # Select the column and 'other_column'
@@ -16,7 +16,7 @@ tree <- function(df, table_name,n = 10) {
     arr.data <- sub_clean_df[order(sub_clean_df[[col_name]]), ]
     
     more_than_0 <- arr.data %>% filter(.data[[col_name]] > 0) %>% tail(n)
-    
+
     less_than_0 <- arr.data %>% filter(.data[[col_name]] < 0) %>% head(n)
     
     up_down <- rbind(more_than_0, less_than_0)
@@ -25,12 +25,10 @@ tree <- function(df, table_name,n = 10) {
                                                   paste("more correlated in", gsub("score|vs.*", "", col_name)),
                                                   ifelse(.data[[col_name]] < 0, paste("more correlated in", sub(".*vs\\s*", "", col_name)),
                                                          "No regulation")))
-    
-    title <- colnames(dff)[1]
-    
+    dff$abs.score <- abs(round(dff[[col_name]],2))
     all_genes <- unique(unlist(dff$preferredNames))
     
-
+    
     binary_matrix <- matrix(0, nrow = nrow(dff), ncol = length(all_genes), dimnames = list(dff$description, all_genes)) 
     n <- 1
     for (i in dff$preferredNames) {
@@ -47,19 +45,28 @@ tree <- function(df, table_name,n = 10) {
     dendrogram_object <- as.dendrogram(hclust_object)
     newick <- as.phylo(dendrogram_object)
     # make dendrogram
-    p1 <- ggtree(newick, branch.length="none", layout="circular")  + 
-      geom_tiplab(align=TRUE, linesize = 0, size = 3, offset = 1)+ ggtitle(title)+
-      theme(plot.title = element_text(hjust = 0))+ xlim(0, 20)
+    p1 <- ggtree(newick, branch.length="none", layout="circular",open.angle=15)  + 
+      geom_tiplab(align=TRUE, linesize = 0, size = 5, offset = off) + ggtitle(col_name)+
+      theme(plot.title = element_text(hjust = 0))+ xlim(0, 50) +
+      theme(plot.margin = margin(1, 1, 1, 1, "in"))
     
     p2 <- p1 + 
       geom_fruit(
         data = dff,
         geom = geom_col,
-        mapping = aes(y=description, x=title, fill=correlated)
+        mapping = aes(y=description, x=abs.score, fill=correlated),
+        axis.params=list(
+          axis       = "x",
+          text.size  = 2,
+          hjust      = 0,
+          vjust      = 0.70,
+          line.size = 0.2, line.alpha = 1.2),
+        grid.params=list()
       ) 
-    ggsave(paste(title,table_name,".svg"), p2, device = "svg",
-           width = 16, height = 14, units = "in", dpi = 500)
     
+    
+    ggsave(paste(col_name,table_name,".svg"), p2, device = "svg",width = 100, height = 98, units = "cm", dpi = 500)
+
   }
 }
 
@@ -67,5 +74,5 @@ table_names <- names(CoPPI_results$resultsCoPPI)
 
 for (table_name in table_names) {
   table_summary <- CoPPI_results$resultsCoPPI[[table_name]]$table_summary
-  tree(table_summary,table_name, 15)
+  tree(table_summary,table_name, 10,2)
 }
